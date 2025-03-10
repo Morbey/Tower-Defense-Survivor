5aba8845c695820f020940eb1770a087bb58a6b5
Eduardo Morbey <eduardomorbey@gmail.com>
2025-03-10 17:06:49 +0000

    Initial project structure

---
+++ game-data-service/src/main/java/com/morfun/towerdefensesurvival/game_data_service/GameDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.morfun.towerdefensesurvival.game_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDataServiceApplication.class, args);
+	}
+
+}
---
+++ game-data-service/src/test/java/com/morfun/towerdefensesurvival/game_data_service/GameDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morfun.towerdefensesurvival.game_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
